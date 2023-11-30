a799edeba4a5f769912ef2836f3d555427f1b48e
root <root@DESKTOP-SKBK36U>
2023-11-29 23:46:58 -0300

    add blog posts

--- app/common/blog.tsx
+++ app/common/blog.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styles from './blog.module.css'
 import Image from 'next/image'
 import { Parallax } from 'react-scroll-parallax'
+import BlogCard from './blogCard'
 
 export default function Blog()   {
 
@@ -10,7 +11,35 @@ export default function Blog()   {
     <>
         <div className={styles.main}>
             <section className={styles.container}>
-                <h5>MAIS ACESSADOS NO NOSSO <span>BLOG</span></h5>
+                <h5>MAIS ACESSADOS NO <span>NOSSO <Image className={styles.blogTitleDetail} src="/squashBlack.svg" width={400} height={20} alt="Blog Luziac - Artigos Imobiliarios"></Image>BLOG</span></h5>
+                <div className={styles.containerCards}>
+                  <BlogCard 
+                    title='LOREM IPSUM'
+                    id={1}
+                    key={1}
+                    srcImage='/blogImage.svg'
+                    description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                  />
+                  <BlogCard 
+                    title='LOREM IPSUM'
+                    id={2}
+                    key={2}
+                    srcImage='/blogImage.svg'
+                    description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                  />
+                  <BlogCard 
+                    title='LOREM IPSUM'
+                    id={3}
+                    key={3}
+                    srcImage='/blogImage.svg'
+                    description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                  />
+                </div>
+                
+                  <button className={styles.MainButton}>
+                    <p>CURTIU? <span>VEJA MAIS ARTIGOS AQUI!</span></p> 
+                  </button>
+               
             </section>
         </div>
     </>

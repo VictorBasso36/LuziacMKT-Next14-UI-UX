d6f40a3541212d412b9e2336d44bab44e5bc42ef
root <root@DESKTOP-SKBK36U>
2023-12-15 19:20:41 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Epilogue } from 'next/font/google'
 import './globals.css'
 import { ParallaxProvider, OpenProvider } from '../app/Blog/providers/providers'
+import Script from 'next/script'
 
 
 const epilogue = Epilogue({ subsets: ['latin-ext'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], variable: '--Epilogue'})
@@ -41,15 +42,27 @@ export default function RootLayout({
   return (
     
     <html lang="pt-br">
-        <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TS6PB2TC"></script>
-        <script dangerouslySetInnerHTML={{
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id='GTM-TS6PB2TC'`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-TS6PB2TC');
-          `
-        }} />
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-TS6PB2TC');
+  `,
+        }}
+      />
         <meta name="robots" content="index, follow"></meta>
         <meta name="revisit-after" content="5 days"></meta>
         <meta name="author" content="Guilherme Luziac, Victor Basso Dev, LUZIAC - CRIANDO CONEXÕES"></meta>
@@ -58,6 +71,19 @@ export default function RootLayout({
       <body className={epilogue.className}>
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TS6PB2TC"
       height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
+       <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-TS6PB2TC');
+  `,
+        }}
+      />
         <ParallaxProvider key={0}>
           <OpenProvider key={0}>
             {children}

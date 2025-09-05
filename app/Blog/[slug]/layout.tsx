287d184c8d7b69bda7f0e4c7dea7510e0a74f177
VictorBasso36 <victorbasso36@gmail.com>
2025-09-05 17:09:35 -0300

    fix: tag

--- app/Blog/[slug]/layout.tsx
+++ app/Blog/[slug]/layout.tsx
@@ -1,87 +1,102 @@
-import type { Metadata } from 'next'
-import { Epilogue } from 'next/font/google'
-import '../../../app/globals.css'
-import { ParallaxProvider, OpenProvider } from '../providers/providers'
-import Script from 'next/script'
-
+import type { Metadata } from "next";
+import { Epilogue } from "next/font/google";
+import "../../../app/globals.css";
+import { ParallaxProvider, OpenProvider } from "../providers/providers";
+import Script from "next/script";
 
 interface PropsGet {
-    slugId: string,
-    id: number,
-    slug: string,
-    description: string,
-    image: string
-  }
-  
-  export async function generateStaticParams() {
-  
-    const request = (await fetch('https://www.luziac.com.br/pages/api/blog')).json() 
-    if(!request) return {}
-    const posts = await request
-    return posts.map((post: any) => ({
-      id: post.id,
-      slugId: post.slugId,
-      slug: post.slug,
-      description: post.description,
-      image: post.image,
-    }))
-  }
-  
-  export async function generateMetadata({ params }: any): Promise<Metadata> { 
-  
-    const slugMetadata = (await fetch('https://www.luziac.com.br/pages/api/blog')).json() 
-    if(!slugMetadata) return {}
-    const data = await slugMetadata
-  
-    const slug = decodeURIComponent(params?.slug)
-    const content = data.find((objeto: any) => objeto.slug === slug.toString());
-    return {
-      title: content?.slug ? `Artigo Imobiliário : ${content?.slug}` : 'Luziac - Marketing Imobiliário',
-      description: content?.description  ? content?.description : 'Luziac - Marketing Imobiliário',
-      metadataBase: new URL('https://www.luziac.com.br/'),
-      robots: {
-        index: true,
-        googleBot: 'index, follow',
-      },
-      
-      authors: [{
-        name: 'Guilherme Luziac',
-        url: 'https://www.linkedin.com/in/guilherme-carvalho-bispo/?originalSubdomain=br'
+  slugId: string;
+  id: number;
+  slug: string;
+  description: string;
+  image: string;
+}
+
+export async function generateStaticParams() {
+  const request = (
+    await fetch("https://www.luziac.com.br/pages/api/blog")
+  ).json();
+  if (!request) return {};
+  const posts = await request;
+  return posts.map((post: any) => ({
+    id: post.id,
+    slugId: post.slugId,
+    slug: post.slug,
+    description: post.description,
+    image: post.image,
+  }));
+}
+
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  const slugMetadata = (
+    await fetch("https://www.luziac.com.br/pages/api/blog")
+  ).json();
+  if (!slugMetadata) return {};
+  const data = await slugMetadata;
+
+  const slug = decodeURIComponent(params?.slug);
+  const content = data.find((objeto: any) => objeto.slug === slug.toString());
+  return {
+    title: content?.slug
+      ? `Artigo Imobiliário : ${content?.slug}`
+      : "Luziac - Marketing Imobiliário",
+    description: content?.description
+      ? content?.description
+      : "Luziac - Marketing Imobiliário",
+    metadataBase: new URL("https://www.luziac.com.br/"),
+    robots: {
+      index: true,
+      googleBot: "index, follow",
+    },
+
+    authors: [
+      {
+        name: "Guilherme Luziac",
+        url: "https://www.linkedin.com/in/guilherme-carvalho-bispo/?originalSubdomain=br",
       },
       {
-        name: 'Victor Basso Dev',
-        url: 'www.linkedin.com/in/victor-basso-b3090a189'
-      }  
-      ],
-      icons: {
-        icon: '/faviicon.svg',
+        name: "Victor Basso Dev",
+        url: "www.linkedin.com/in/victor-basso-b3090a189",
       },
-    }
-  }
+    ],
+    icons: {
+      icon: "/faviicon.svg",
+    },
+  };
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode,
+  children: React.ReactNode;
 }) {
   return (
-    <><Script
-      id="gtm-script"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', 'GTM-TS6PB2TC');
-`,
-      }} /><ParallaxProvider key={0}>
-        <OpenProvider key={1}>
-          {children}
-        </OpenProvider>
+    <>
+      {" "}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N5RD4HDL"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N5RD4HDL');
+  `,
+        }}
+      />
+      <ParallaxProvider key={0}>
+        <OpenProvider key={1}>{children}</OpenProvider>
       </ParallaxProvider>
-      </> 
-
- )
+    </>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,88 +1,106 @@
-import type { Metadata } from 'next'
-import { Epilogue } from 'next/font/google'
-import './globals.css'
-import { ParallaxProvider, OpenProvider } from '../app/Blog/providers/providers'
-import Script from 'next/script'
+import type { Metadata } from "next";
+import { Epilogue } from "next/font/google";
+import "./globals.css";
+import {
+  ParallaxProvider,
+  OpenProvider,
+} from "../app/Blog/providers/providers";
+import Script from "next/script";
 
+const epilogue = Epilogue({
+  subsets: ["latin-ext"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--Epilogue",
+});
 
-const epilogue = Epilogue({ subsets: ['latin-ext'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], variable: '--Epilogue'})
-
-
-export async function generateMetadata(): Promise<Metadata> { 
-
+export async function generateMetadata(): Promise<Metadata> {
   return {
-    
-    authors: [{
-      name: 'Guilherme Luziac',
-      url: 'https://www.linkedin.com/in/guilherme-carvalho-bispo/?originalSubdomain=br'
-    },
-    {
-      name: 'Victor Basso Dev',
-      url: 'www.linkedin.com/in/victor-basso-b3090a189'
-    }  
+    authors: [
+      {
+        name: "Guilherme Luziac",
+        url: "https://www.linkedin.com/in/guilherme-carvalho-bispo/?originalSubdomain=br",
+      },
+      {
+        name: "Victor Basso Dev",
+        url: "www.linkedin.com/in/victor-basso-b3090a189",
+      },
     ],
-    title: 'LUZIAC - Marketing Imobiliário, criando conexões | Luziac, criando conexões.',
-    description: 'Ajudando imobiliárias e incorporadoras a venderem mais com estratégias inteligentes.',
-    keywords: ['marketing imobiliário', 'imobiliárias', 'incorporadoras', 'vendas estratégicas', 'sites imobiliários', 'marketing digital imobiliário', 'identidade visual', 'anúncios imobiliários', 'gestão imobiliária', 'captação imobiliária', 'edição imobiliária', 'sites para imobiliárias'],
+    title:
+      "LUZIAC - Marketing Imobiliário, criando conexões | Luziac, criando conexões.",
+    description:
+      "Ajudando imobiliárias e incorporadoras a venderem mais com estratégias inteligentes.",
+    keywords: [
+      "marketing imobiliário",
+      "imobiliárias",
+      "incorporadoras",
+      "vendas estratégicas",
+      "sites imobiliários",
+      "marketing digital imobiliário",
+      "identidade visual",
+      "anúncios imobiliários",
+      "gestão imobiliária",
+      "captação imobiliária",
+      "edição imobiliária",
+      "sites para imobiliárias",
+    ],
     icons: {
-      icon: '/faviicon.svg',
+      icon: "/faviicon.svg",
     },
     robots: {
       index: true,
-      googleBot: 'index, follow',
-    }
-  }
+      googleBot: "index, follow",
+    },
+  };
 }
 
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.luziac.com.br/',
+      url: "https://www.luziac.com.br/",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: 'https://www.luziac.com.br/Blog/Como%20a%20Realidade%20Virtual%20Impacta%20na%20Venda%20de%20Im%C3%B3veis%20na%20Planta.',
+      url: "https://www.luziac.com.br/Blog/Como%20a%20Realidade%20Virtual%20Impacta%20na%20Venda%20de%20Im%C3%B3veis%20na%20Planta.",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: 'https://www.luziac.com.br/Blog/Como%20as%20fotos%20dos%20seus%20im%C3%B3veis%20impactam%20no%20engajamento%20das%20suas%20redes.https://www.luziac.com.br/Blog/Como%20as%20fotos%20dos%20seus%20im%C3%B3veis%20impactam%20no%20engajamento%20das%20suas%20redes.',
+      url: "https://www.luziac.com.br/Blog/Como%20as%20fotos%20dos%20seus%20im%C3%B3veis%20impactam%20no%20engajamento%20das%20suas%20redes.https://www.luziac.com.br/Blog/Como%20as%20fotos%20dos%20seus%20im%C3%B3veis%20impactam%20no%20engajamento%20das%20suas%20redes.",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.5,
     },
 
     {
-      url: 'https://www.luziac.com.br/Blog/Como%20a%20Queda%20da%20Taxa%20Selic%20Interfere%20nas%20Suas%20Vendas%20de%20Im%C3%B3veis',
+      url: "https://www.luziac.com.br/Blog/Como%20a%20Queda%20da%20Taxa%20Selic%20Interfere%20nas%20Suas%20Vendas%20de%20Im%C3%B3veis",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.5,
     },
-  ]
+  ];
 }
 
-sitemap()
+sitemap();
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode,
+  children: React.ReactNode;
 }) {
   return (
-    
     <html lang="pt-br">
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id='GTM-TS6PB2TC'`}
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N5RD4HDL"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
-        />
+        ></iframe>
       </noscript>
       <Script
         id="gtm-script"
@@ -93,38 +111,44 @@ export default function RootLayout({
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', 'GTM-TS6PB2TC');
+    })(window,document,'script','dataLayer','GTM-N5RD4HDL');
   `,
         }}
       />
-        <meta name="robots" content="index, follow"></meta>
-        <meta name="revisit-after" content="5 days"></meta>
-        <meta name="author" content="Guilherme Luziac, Victor Basso Dev, LUZIAC - CRIANDO CONEXÕES"></meta>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1" />
-        <meta name="language" content="pt-br"></meta> 
+      <meta name="robots" content="index, follow"></meta>
+      <meta name="revisit-after" content="5 days"></meta>
+      <meta
+        name="author"
+        content="Guilherme Luziac, Victor Basso Dev, LUZIAC - CRIANDO CONEXÕES"
+      ></meta>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1" />
+      <meta name="language" content="pt-br"></meta>
       <body className={epilogue.className}>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TS6PB2TC"
-      height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
-       <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TS6PB2TC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer', 'GTM-TS6PB2TC');
   `,
-        }}
-      />
+          }}
+        />
         <ParallaxProvider key={0}>
-          <OpenProvider key={0}>
-            {children}
-          </OpenProvider>
+          <OpenProvider key={0}>{children}</OpenProvider>
         </ParallaxProvider>
-        
-        </body>
+      </body>
     </html>
-  )
+  );
 }
